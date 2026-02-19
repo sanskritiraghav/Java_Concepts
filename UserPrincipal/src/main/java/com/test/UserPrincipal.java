f29c8542b037a8d4d3cd846331c0689765401a24
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-19 12:47:54 +0530

    new commit

---
+++ UserPrincipal/src/main/java/com/test/UserPrincipal.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class UserPrincipal {
+	private String name;
+	public UserPrincipal(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String getName() {
+		return null;
+	}
+	
+	public void setName() {
+		this.name = name;
+	}
+	
+	public String toString() {
+		
+	}
+}
