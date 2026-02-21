d7bf57a1f3dac13c5f6492ee2441cb4359127826
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-21 12:05:30 +0530

    Mapping commit

---
+++ HibernateMappingExp2/src/main/java/com/test/TestDemo.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction t = session.beginTransaction();
+		Order o1 = new Order();
+		o1.setO_name("Orange");
+		o1.setO_price("5");
+		
+		Order o2 = new Order();
+		o2.setO_name("Apple");
+		o2.setO_price("10");
+		
+		List<Order> list = Arrays.asList(o1,o2);
+		
+		Customer cst = new Customer();
+		cst.setC_name("Sanskriti");
+		cst.setOrders(list);
+		
+		session.persist(cst);
+		t.commit();
+		session.close();
+	}
+}
