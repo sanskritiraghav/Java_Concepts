84bcfaef689f6ec56dc235fdffff184cf080ca5a
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-25 15:47:29 +0530

    MLATeamDAO1 file added

--- SpringJDBExp1/src/main/java/com/test/main/TestDemo.java
+++ SpringJDBExp1/src/main/java/com/test/main/TestDemo.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.test.dao.MLATeamDAO1;
 import com.test.dao.MLATeamDao;
 import com.test.model.MLATeam;
 
@@ -13,12 +14,16 @@ public class TestDemo {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 		MLATeamDao dao = ctx.getBean("tdao", MLATeamDao.class);
+		
+		MLATeamDAO1 dao1 = ctx.getBean("tdao1", MLATeamDAO1.class);
+		
 		MLATeam obj = new MLATeam();
-		obj.setId(101);
-		obj.setName("Sanskriti");
-		obj.setSkill("Java Developer");
-		obj.setManager("Arjun");
-		dao.saveMLATeam(obj);
+		obj.setId(1226);
+		obj.setName("Amit");
+		obj.setSkill("Python Developer");
+		obj.setManager("Pavan");
+		dao1.createMLATeam(obj);
+		//dao.saveMLATeam(obj);
 		//dao.updateMLATeam(obj);
 		//dao.deleteMLATeamMember(obj);
 		List<MLATeam> list = dao.getAllMembers();
@@ -30,4 +35,3 @@ public class TestDemo {
 	}
 
 }
-
