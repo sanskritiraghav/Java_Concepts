7be1ac899be0fa3ffc7842e533c8976c0a0fef79
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-25 10:49:16 +0530

    AOP concept commit

---
+++ SpringAOPExp1/src/main/java/com/test/spring/aspect/EmployeeAspectJoinPoint.java
@@ -0,0 +1,20 @@
+package com.test.spring.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class EmployeeAspectJoinPoint {
+	@Before("execution(public void com.test.model..set*(*))")
+	public void loggingAdive(JoinPoint joinPoint) {
+		System.out.println("before executing loggingAdvice on method : "+joinPoint.toString());
+		System.out.println("arguments passes : "+Arrays.toString(joinPoint.getArgs()));
+	}
+	@Before("args(name)")
+	public void logStringArguments(String name) {
+		System.out.println("String argument passed : "+name);
+	}
+}
