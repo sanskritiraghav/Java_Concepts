df84668947255b69c7fd74dafebc6d86b132495a
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-26 10:30:11 +0530

    greeting response added

--- RestfulWebService/src/main/java/com/test/HelloService.java
+++ RestfulWebService/src/main/java/com/test/HelloService.java
@@ -3,7 +3,9 @@ package com.test;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
@@ -13,5 +15,16 @@ public class HelloService {
 	@Path("/text")
 	public String simplePlainTestResponse() {
 		return "This is simple Restful web service Plain text Response";
+	}
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Path("/html/{debit}")
+	public String htmlTextResponse(@PathParam("debit") String card) {
+		return "<html><body> <h1> Simple RestAPI HTML Response card no: "+card+" </h1> </body></html>";
+	}
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String greetUserResponse(@QueryParam("msg") String msg) {
+		return msg;
 	}
 }
--- RestfulWebService/src/main/webapp/index.jsp
+++ RestfulWebService/src/main/webapp/index.jsp
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h2><%= "Hello World! RestAPI Example" %></h2>
-	<a href = "rest/hello/text">click here</a>
+	<a href = "rest/hello/text">click text</a><br><br>
+	<a href = "rest/hello/html">click html</a>
 </body>
 </html>
