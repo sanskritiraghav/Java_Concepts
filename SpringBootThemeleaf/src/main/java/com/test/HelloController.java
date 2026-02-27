85a422b2d120b3260b8abd5b96d21e4ba5f6f38a
sanskritiraghav <sanskritiraghav01@gmail.com>
2026-02-27 09:36:04 +0530

    thymeleaf commit

---
+++ SpringBootThemeleaf/src/main/java/com/test/HelloController.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/")
+	public String simpleHomePage(Model model) {
+		model.addAttribute("msg","WELCOME TO THYMELEAF");
+		return "index";
+	}
+}
